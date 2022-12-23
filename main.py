dcc9a99d16e9fa81558065fb6f9dc96b53eba4ee
Miron <miron@users.noreply.github.com>
2022-12-23 18:20:36 +0000

    optimized, loop on open file

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ if not os.path.exists("data.shelve"):
     with shelve.open("data.shelve") as data:
     # Initialize the prompts list if it doesn not exist
         data["prompts"] = []
-while True:
-    text = input("Enter a prompt (or 'q' to quit): ")
-    if text.lower() == "q":
-        break
-    # Open the Shelve file for writing
-    with shelve.open("data.shelve") as data:
+# Open the Shelve file for reading and writing
+with shelve.open("data.shelve") as data:
+    prompts = data["prompts"]
+    while True:
+        text = input("Enter a prompt (or 'q' to quit): ")
+        if text.lower() == "q":
+            break
         # Add a new prompt to the Shelve file
-        prompts = data["prompts"]
         id = len(prompts) + 1
         prompt = {"id": id, "text": text, "sentiment": "neutral"}
         prompts.append(prompt)
         data["prompts"] = prompts
-    with shelve.open("data.shelve", flag="r") as data:
-        prompts = data["prompts"]
 
         # print the data
         print("Prompts:")
